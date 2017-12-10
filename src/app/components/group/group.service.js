015345954252014249b0e15bca2b9f93e358b89b
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-12-10 18:18:15 +0200

    Created GroupService for returning data

---
+++ src/app/components/group/group.service.js
@@ -0,0 +1,28 @@
+export class GroupService {
+  constructor() {
+    'ngInject';
+
+    this.groups = [
+      {
+        name: "КТ-5",
+        students: [
+          {fullName: "Ivan Yarymovych", email: "ivan11yarymovych@gmail.com", age: 23},
+          {fullName: "Vlad Dzhus", email: "vlad.dzhus@gmail.com", age: 24},
+          {fullName: "Natali Kovalchuk", email: "nata@gmail.com", age: 22}
+        ]
+      },
+      {
+        name: "АУ-5",
+        students: [
+          {fullName: "Orest Denyshyn", email: "o.d.@gmail.com", age: 21},
+          {fullName: "Slava Shkilnyi", email: "s.s.@gmail.com", age: 24},
+          {fullName: "Roma Chychkevych", email: "roma@gmail.com", age: 22}
+        ]
+      }
+    ];
+  }
+
+  getGroups() {
+    return this.groups;
+  }
+}
--- src/app/index.module.js
+++ src/app/index.module.js
@@ -3,12 +3,15 @@ import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
 import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
+import { GroupService } from '../app/components/group/group.service';
 import { NavbarDirective } from '../app/components/navbar/navbar.directive';
+
 
 angular.module('groupStudent', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'mgcrea.ngStrap', 'toastr'])
   .config(config)
   .config(routerConfig)
   .run(runBlock)
   .service('webDevTec', WebDevTecService)
+  .service('group', GroupService)
   .controller('MainController', MainController)
   .directive('navbar', NavbarDirective)
