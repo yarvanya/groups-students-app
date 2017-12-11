11b77a8f34c20fc1626f8ed5f50d2bb2db2979e4
Ivan Yarymovych <iyary@softserveinc.com>
2017-12-11 14:43:40 +0200

    Added simple alert message after creating new group

--- src/app/components/group/group.directive.js
+++ src/app/components/group/group.directive.js
@@ -13,21 +13,32 @@ export function GroupDirective() {
 }
 
 export class GroupController {
-  constructor(groups) {
+  constructor(groups, $alert) {
     'ngInject';
 
     this.groupsList = [];
     this.newGroup = {};
     this.getGroupList(groups);
+    this.groupAlert = $alert;
   }
 
   getGroupList(groups) {
     this.groupsList = groups.getGroups();
   }
 
+  openGroupAlert() {
+    return this.groupAlert({
+      title: 'Success!',
+      content: 'You have just successfully created new group!',
+      type: 'success',
+      duration: 5
+    });
+  }
+
   createNewGroup() {
     const assignGroup = Object.assign({}, this.newGroup, {students: []});
     this.groupsList.push(assignGroup);
+    this.openGroupAlert();
   }
 
 }
