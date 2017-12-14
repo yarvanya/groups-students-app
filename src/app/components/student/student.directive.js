f609335527b8283887c856d39c7fe3df1a9f2867
Ivan Yarymovych <iyary@softserveinc.com>
2017-12-14 13:43:00 +0200

    Changed all logic in student component and added validations

--- src/app/components/student/student.directive.js
+++ src/app/components/student/student.directive.js
@@ -16,31 +16,27 @@ export class StudentController {
   constructor(groups, students, $alert) {
     'ngInject';
 
-    this.disabledUpdateButton = true;
-    this.groupsList = groups.groups;
-    this.studentsList = students.students;
-    angular.forEach(students.students, (student) => {
-      angular.forEach(groups.groups, (group) => {
+    this.groupsList = groups;
+    this.studentsList = students;
+    this.renderStudentsData();
+    this.newStudent = {};
+    this.editedStudent = {};
+    this.studentAlert = $alert;
+  }
+
+  renderStudentsData() {
+    angular.forEach(this.studentsList, (student) => {
+      angular.forEach(this.groupsList, (group) => {
         if (student.groupId === group.id) {
           student.group = group.name;
         }
       });
     });
-    this.newStudent = {};
-    this.studentAlert = $alert;
   }
 
   selectStudent(student) {
     this.clickedStudent = student;
-  }
-
-  editStudentFieldValidation(student) {
-    if (student.fullName && student.email && student.age) {
-      this.clickedStudent = student;
-      this.disabledUpdateButton = false;
-    } else {
-      this.disabledUpdateButton = true;
-    }
+    this.editedStudent = Object.assign({}, this.clickedStudent);
   }
 
   openStudentAlert(content) {
@@ -59,21 +55,30 @@ export class StudentController {
     return true;
   }
 
+  updateStudentFieldValidation(editedStudent) {
+    this.clickedStudentForValidate = Object.assign({}, this.clickedStudent, editedStudent);
+    if (this.clickedStudentForValidate.fullName && this.clickedStudentForValidate.email && this.clickedStudentForValidate.age) {
+      return false;
+    }
+    return true;
+  }
+
   createStudent(group) {
     const studentListLength = this.studentsList.length;
-    console.log("group: ", group);
-    const assignStudent = Object.assign({}, this.newStudent, {studentId: this.studentsList[studentListLength - 1].studentId + 1, groupId: group.groupId, })
-    console.log("assignStudent: ", assignStudent);
+    const assignStudent = Object.assign({}, {id: this.studentsList[studentListLength - 1].id + 1, groupId: group.id, }, this.newStudent)
     this.studentsList.push(assignStudent);
+    this.renderStudentsData();
+    this.openStudentAlert(`You have just successfully created student: ${this.newStudent.fullName}`);
     this.newStudent = {};
   }
 
-  updateStudent(student) {
-    this.openStudentAlert(`You have just successfully updated student: ${student.fullName}`);
+  updateStudent(editedStudent) {
+    this.clickedStudent = Object.assign(this.clickedStudent, editedStudent);
+    this.openStudentAlert(`You have just successfully updated student: ${this.clickedStudent.fullName}`);
   }
 
-  deleteStudent(group) {
-    group.students.splice(group.students.indexOf(this.clickedStudent), 1);
+  deleteStudent(student) {
+    this.studentsList.splice(this.studentsList.indexOf(student), 1);
     this.openStudentAlert(`You have just successfully deleted student: ${this.clickedStudent.fullName}`);
   }
 }
--- src/app/components/student/student.service.js
+++ src/app/components/student/student.service.js
@@ -2,17 +2,14 @@ export class StudentService {
   constructor() {
     'ngInject';
 
-    this.students = [
-      {studentId: 1, groupId: 1, fullName: "Ivan Yarymovych", email: "ivan11yarymovych@gmail.com", age: 23},
-      {studentId: 2, groupId: 1, fullName: "Vlad Dzhus", email: "vlad.dzhus@gmail.com", age: 24},
-      {studentId: 3, groupId: 1, fullName: "Natali Kovalchuk", email: "nata@gmail.com", age: 22},
-      {studentId: 4, groupId: 2, fullName: "Orest Denyshyn", email: "o.d.@gmail.com", age: 21},
-      {studentId: 5, groupId: 2, fullName: "Slava Shkilnyi", email: "s.s.@gmail.com", age: 24},
-      {studentId: 6, groupId: 2, fullName: "Roma Chychkevych", email: "roma@gmail.com", age: 22}
+    return this.students = [
+      {id: 1, groupId: 1, fullName: "Ivan Yarymovych", email: "ivan11yarymovych@gmail.com", age: 23},
+      {id: 2, groupId: 1, fullName: "Vlad Dzhus", email: "vlad.dzhus@gmail.com", age: 24},
+      {id: 3, groupId: 1, fullName: "Natali Kovalchuk", email: "nata@gmail.com", age: 22},
+      {id: 4, groupId: 2, fullName: "Orest Denyshyn", email: "o.d.@gmail.com", age: 21},
+      {id: 5, groupId: 2, fullName: "Slava Shkilnyi", email: "s.s.@gmail.com", age: 24},
+      {id: 6, groupId: 2, fullName: "Roma Chychkevych", email: "roma@gmail.com", age: 22},
+      {id: 7, groupId: 1, fullName: "Anna Karnasevych", email: "annakar@gmail.com", age: 22}
     ];
-  }
-
-  getStudents() {
-    return this.students;
   }
 }
