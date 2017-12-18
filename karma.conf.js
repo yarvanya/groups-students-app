64d4b6e5d6d36d7f8d410b10b32ba791b6f8e468
Ivan Yarymovych <iyary@softserveinc.com>
2017-12-18 13:13:51 +0200

    Changed karma config file

--- karma.conf.js
+++ karma.conf.js
@@ -24,6 +24,15 @@ module.exports = function (config) {
           }
         ]
       ]
+    },
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
     }
   });
+  if (process.env.TRAVIS) {
+    configuration.browsers = ['Chrome_travis_ci'];
+  }
 };
