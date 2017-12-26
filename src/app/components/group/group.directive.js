c3cb1a2995a7266aaedafe38bd3e725ab5853622
Ivan Yarymovych <iyary@softserveinc.com>
2017-12-26 11:53:32 +0200

    Created separate modal for creating/editing groups

--- src/app/components/group/group.directive.js
+++ src/app/components/group/group.directive.js
@@ -20,8 +20,6 @@ export class GroupController {
 
     this.groupsList = groups;
     this.studentsList = students;
-    this.newGroup = {};
-    this.editedGroup = {};
     this.alert = $alert;
     this.groupScope = $scope;
     this.modalParams = {};
@@ -29,8 +27,8 @@ export class GroupController {
 
   selectDataForCreating() {
     this.eventName = "Creating";
-
-    this.data = {
+    this.newGroup = {};
+    this.modalParams = {
       eventName: this.eventName,
       model: this.newGroup,
       groupsList: this.groupsList,
@@ -46,12 +44,15 @@ export class GroupController {
     this.eventName = "Updating";
     this.clickedGroup = group;
     this.editedGroup = Object.assign({}, this.clickedGroup);
-
-    this.data = {
+    this.modalParams = {
       eventName: this.eventName,
-      model: this.clickedGroup,
+      model: this.editedGroup,
+      clickedGroup: this.clickedGroup,
+      groupsList: this.groupsList,
       validation: this.validation,
-      event: this.updateGroup,
+      action: this.updateGroup,
+      alert: this.alert,
+      alertOpen: this.openGroupAlert,
       buttonName: "Update"
     };
   }
@@ -85,7 +86,6 @@ export class GroupController {
   }
 
   createNewGroup() {
-    console.log(this);
     const groupsListLength = this.groupsList.length;
     const assignGroup = Object.assign({},
       {id: this.groupsList[groupsListLength - 1].id + 1},
@@ -93,12 +93,11 @@ export class GroupController {
     );
     this.groupsList.push(assignGroup);
     this.alertOpen(`${messages.createGroup} ${this.model.name}`);
-    this.model = {};
   }
 
   updateGroup(editedGroup) {
     this.clickedGroup = Object.assign(this.clickedGroup, editedGroup);
-    this.openGroupAlert(`${messages.updateGroup} ${this.clickedGroup.name}`);
+    this.alertOpen(`${messages.updateGroup} ${this.model.name}`);
   }
 
   deleteGroup(group) {
